4ba0a9a6c6acd5f6bdd95d500677bee665195b69
joel ambu <ambujoel@gmail.com>
2021-07-01 17:29:11 +0100

    Compress css files

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            options: {
+              mergeIntoShorthands: false,
+              roundingPrecision: -1
+            },
+            target: {
+              files: {
+                'public/css/output.css': 
+                    [
+                      'public/vendor/animate/animate.min.css',
+                      'public/vendor/bootstrap/css/bootstrap.min.css',
+                      'public/css/stylesheet.min.css',
+                    ]
+              }
+            }
+          }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+}
